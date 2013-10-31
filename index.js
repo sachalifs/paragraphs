5b9efcd6d74b1082d8e7d7ad46457e20465139a5
slifszyc <sacha.lifszyc@gmail.com>
2013-10-31 15:29:23 -0300

    Removed old exports

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 /**
- * Expose browser constructors
- */
-
-exports.ie = require('./lib/ie');
-exports.ff = require('./lib/ff');
-exports.chrome = require('./lib/chrome');
-exports.safari = require('./lib/safari');
-exports.mac = require('./lib/mac');
-
-/**
  * Expose Paragraph constructor
  */
 
